89b7c170507b7eadff138a6dbb42e2062f880d2e
Sankalp Mishra <mishrasankalp38@gmail.com>
2026-05-27 13:30:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (98.14%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -16,22 +16,22 @@ public:
             for(int i = left; i <= right; i++) {
                 ans.push_back(matrix[top][i]);
             }
-            top++;
+            ++top;
             for(int i = top; i <= bottom; i++) {
                 ans.push_back(matrix[i][right]);
             }
-            right--;
+            --right;
             if(top <= bottom) {
                 for(int i = right; i >= left; i--) {
                     ans.push_back(matrix[bottom][i]);
                 }
-                bottom--;
+                --bottom;
             }
             if(left <= right) {
                 for(int i = bottom; i >= top; i--) {
                     ans.push_back(matrix[i][left]);
                 }
-                left++;
+                ++left;
             }
         }
 
