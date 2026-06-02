0464a11d11939ee2600441f114c72786e3d288b5
Sankalp Mishra <mishrasankalp38@gmail.com>
2026-06-02 21:06:01 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (12.33%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.cpp
+++ 3321-type-of-triangle/type-of-triangle.cpp
@@ -2,21 +2,33 @@ class Solution {
 public:
     string triangleType(vector<int>& nums) {
 
-        if(nums[0] + nums[1] <= nums[2] ||
-           nums[1] + nums[2] <= nums[0] ||
-           nums[0] + nums[2] <= nums[1])
-        {
-            return "none";
+        string ch = "equilateral";
+        string ch1 = "isosceles";
+        string ch2 = "scalene";
+        string ch3 = "none";
+
+        if(nums[0] == nums[1] && nums[1] == nums[2]){
+            return ch;
         }
-
-        if(nums[0] == nums[1] && nums[1] == nums[2])
-            return "equilateral";
-
-        if(nums[0] == nums[1] ||
-           nums[1] == nums[2] ||
-           nums[0] == nums[2])
-            return "isosceles";
-
-        return "scalene";
+        else if(nums[0] != nums[1] && nums[1] != nums[2] && nums[0] != nums[2]){
+            if(nums[0] + nums[1] > nums[2] &&
+               nums[1] + nums[2] > nums[0] &&
+               nums[0] + nums[2] > nums[1]){
+                return ch2;
+            }
+            else{
+                return ch3;
+            }
+        }
+        else{
+            if(nums[0] + nums[1] > nums[2] &&
+               nums[1] + nums[2] > nums[0] &&
+               nums[0] + nums[2] > nums[1]){
+                return ch1;
+            }
+            else{
+                return ch3;
+            }
+        }
     }
 };
