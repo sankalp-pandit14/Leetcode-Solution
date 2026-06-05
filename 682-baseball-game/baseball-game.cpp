b62203ec208138e1b15da26e943a0ccb9787c06f
Sankalp Mishra <mishrasankalp38@gmail.com>
2026-06-05 11:40:16 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (62.16%) - LeetSync

---
+++ 682-baseball-game/baseball-game.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int calPoints(vector<string>& operations) {
+        vector<int>arr;
+        int n= operations.size();
+        int finalSum=0;
+        for(int i=0;i<n;i++){
+            if(operations[i]=="C"){
+              arr.pop_back();//  pop back does not take anny aruements it just removes you cannot give task to it to remove a specific use slice for it instead
+            }
+            else if(operations[i]=="+"){
+                int sum= arr[arr.size()-1]+ arr[arr.size()-2];
+                arr.push_back(sum);
+                sum=0;
+            }
+            else if(operations[i]=="D"){
+                int pod= 2*arr[arr.size()-1];
+                arr.push_back(pod);
+                 pod=1;
+            }
+            else{
+                
+                 arr.push_back(stoi(operations[i]));// string to integer converter
+            }
+
+        }
+        for(int i=0;i<arr.size();i++){
+            finalSum+=arr[i];
+        }
+        return finalSum;
+    }
+};
