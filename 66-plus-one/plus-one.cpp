fcce4a080dd7d3330aa9ba691a09b58f52b360d7
Sankalp Mishra <mishrasankalp38@gmail.com>
2026-06-12 00:11:04 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (86.36%) - LeetSync

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -1,10 +1,40 @@
+// class Solution {
+// public:
+//     vector<int> plusOne(vector<int>& digits) {
+//         int n = digits.size();
+//         long long sum = 0;
+//         long long ans = 0;
+//         vector<int> final;
+
+//         for (int i = 0; i < n; i++) {
+//             sum = sum * 10 + digits[i];
+//         }
+
+//         ans = sum + 1;
+
+//         if (ans == 0) {    
+//             final.push_back(0);
+//             return final;
+//         }
+
+//         while (ans > 0) {
+//             final.push_back(ans % 10);
+//             ans /= 10;
+//         }
+
+//         reverse(final.begin(), final.end());
+//         return final;
+//     }
+// };
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
 
-        for (int i = digits.size() - 1; i >= 0; i--) {
+        int n = digits.size();
 
-            if (digits[i] < 9) {
+        for(int i = n - 1; i >= 0; i--) {
+
+            if(digits[i] < 9) {
                 digits[i]++;
                 return digits;
             }
@@ -12,7 +42,13 @@ public:
             digits[i] = 0;
         }
 
-        digits.insert(digits.begin(), 1);
-        return digits;
+        vector<int> final;
+        final.push_back(1);
+
+        for(int i = 0; i < n; i++) {
+            final.push_back(digits[i]);
+        }
+
+        return final;
     }
 };
