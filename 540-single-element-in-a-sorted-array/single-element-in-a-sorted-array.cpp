c10da3ddf4c12ea2661e8086da7232f62fa18a3e
Sankalp Mishra <mishrasankalp38@gmail.com>
2026-06-18 01:29:14 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (33.77%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -1,27 +1,55 @@
+// class Solution {
+// public:
+//     int singleNonDuplicate(vector<int>& nums) {
+//         int n= nums.size();
+//         int ans=0;
+//        if(n==1){
+//     return nums[0];
+// }
+//         for(int i=0;i<n;i++){
+//             if(i==0){
+//                 if(nums[i]!=nums[i+1]){
+//                     ans=nums[i];
+//                     break;}}
+//             else if(i==n-1){
+//                 if(nums[i]!=nums[i-1]){
+//                     ans= nums[i];
+//                     break;}}
+//                     else{
+//                         if(nums[i]!=nums[i+1]&& nums[i-1]!=nums[i]){
+//                             ans= nums[i];
+//                             break;
+//                         }
+//                     }
+//         }
+//         return ans;
+  //  }
+//};
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n= nums.size();
-        int ans=0;
-       if(n==1){
-    return nums[0];
-}
-        for(int i=0;i<n;i++){
-            if(i==0){
-                if(nums[i]!=nums[i+1]){
-                    ans=nums[i];
-                    break;}}
-            else if(i==n-1){
-                if(nums[i]!=nums[i-1]){
-                    ans= nums[i];
-                    break;}}
-                    else{
-                        if(nums[i]!=nums[i+1]&& nums[i-1]!=nums[i]){
-                            ans= nums[i];
-                            break;
-                        }
-                    }
+        int n = nums.size();
+
+        if(n == 1) return nums[0];
+
+        for(int i = 0; i < n; i++) {
+            if(i == 0) {
+                if(nums[i] != nums[i + 1]) {
+                    return nums[i];
+                }
+            }
+            else if(i == n - 1) {
+                if(nums[i] != nums[i - 1]) {
+                    return nums[i];
+                }
+            }
+            else {
+                if(nums[i] != nums[i + 1] && nums[i] != nums[i - 1]) {
+                    return nums[i];
+                }
+            }
         }
-        return ans;
+
+        return -1;
     }
 };
