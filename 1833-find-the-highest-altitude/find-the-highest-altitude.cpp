4066686391e86fe16adc3ba6ae8f47f76ce0e1d2
Sankalp Mishra <mishrasankalp38@gmail.com>
2026-06-20 20:35:12 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (73.80%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -5,11 +5,8 @@ public:
         int maxi=0;
         int sum=0;
         for(int i=0;i<n;i++){
-         maxi= max(maxi,sum);
-         sum+=gain[i];
-           
-        }
-         maxi= max(maxi,sum);
+        sum+=gain[i];
+         maxi= max(maxi,sum);}
         return maxi;
     }
 };
