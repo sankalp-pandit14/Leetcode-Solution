fdd108951caa8ef20bb609f089099ecd53b16505
Sankalp Mishra <mishrasankalp38@gmail.com>
2026-07-20 17:50:52 +0530

    Time: 6 ms (13.81%) | Memory: 16.1 MB (20.58%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> ans;
+
+    void f(string s, int open, int close, int n) {
+        if (s.size() == 2 * n) {
+            ans.push_back(s);
+            return;
+        }
+
+        if (open < n) {
+            f(s + '(', open + 1, close, n);
+        }
+
+        if (close < open) {
+            f(s + ')', open, close + 1, n);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        ans.clear();
+        f("", 0, 0, n);
+        return ans;
+    }
+};
